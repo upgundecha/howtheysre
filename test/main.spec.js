6e0a74705866bd975266c2ba0e146f6f98d477f8
Unmesh Gundecha <upgundecha@gmail.com>
2021-02-21 21:08:36 +0800

    Added uniqueness check

--- test/main.spec.js
+++ test/main.spec.js
@@ -38,4 +38,29 @@ describe('Checklist', function () {
       expect(orgList).to.be.sorted(Intl.Collator().compare);
     });
   });
+
+  describe('Uniqueness', function() {
+    it('has unique items', function () {
+      var items = srcMd.match(/(?<=\* )(.*?)(?=\))/g);
+      var hasDuplicate = items.some((val, i) => items.indexOf(val) !== i);
+      expect(hasDuplicate).to.equal(false, 'List has duplicate items');
+    });
+    it('has unique link text', function () {
+      var items = srcMd.match(/(?<=\* \[)(.*?)(?=\])/g);
+      var hasDuplicate = items.some((val, i) => items.indexOf(val) !== i);
+      expect(hasDuplicate).to.equal(false, 'List has duplicate text');
+    });
+    it('has unique urls', function () {
+      var items = srcMd.match(/(?<=\* \[*.*\]\()(.*?)(?=\))/g);
+      var items = arrayRemove(items, 'https://github.com/abhivaikar/howtheytest');
+      var hasDuplicate = items.some((val, i) => items.indexOf(val) !== i);
+      expect(hasDuplicate).to.equal(false,  'List has duplicate link');
+    });
+  })
 });
+
+function arrayRemove(arr, value) { 
+  return arr.filter(function(ele){ 
+      return ele != value; 
+  });
+}
