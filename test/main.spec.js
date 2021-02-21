5d0dd058c288007558625ca7f4375a9d363963b9
Unmesh Gundecha <upgundecha@gmail.com>
2021-02-21 17:50:23 +0800

    Quick improvements to test

--- test/main.spec.js
+++ test/main.spec.js
@@ -8,32 +8,34 @@ var expect = chai.expect;
 
 const srcMd = fs.readFileSync('README.md', 'utf8');
 
-describe('Header', function () {
-  it('is intact', function () {
-    expect(srcMd).to.startsWith('# How they SRE');
+describe('Checklist', function () {
+  describe('Header', function () {
+    it('is intact', function () {
+      expect(srcMd).to.startsWith('# How they SRE');
+    });
   });
-});
 
-describe('Section Headers', function () {
-  it('intact', function () {
-    var expectedH2List = [
-                          '## Introduction',
-                          '## Organizations',
-                          '## SRECon Mix Playlist',
-                          '## Resources',
-                          '## Credits',
-                          '## Other How They... repos',
-                          '## Contribute',
-                          '## License'
-                        ]
-    var actualList = srcMd.match(/^## (.*$)/gim);
-    expect(expectedH2List).to.equalTo(actualList)
+  describe('Section Headers', function () {
+    it('intact', function () {
+      var expectedH2List = [
+        '## Introduction',
+        '## Organizations',
+        '## SRECon Mix Playlist',
+        '## Resources',
+        '## Credits',
+        '## Other How They... repos',
+        '## Contribute',
+        '## License'
+      ]
+      var actualList = srcMd.match(/^## (.*$)/gim);
+      expect(actualList).to.equalTo(expectedH2List)
+    });
   });
-});
 
-describe('Organization list', function () {
-  it('is sorted', function () {
-    var orgList = srcMd.match(/(?<=<summary>)(.*?)(?=<\/summary>)/g);
-    expect(orgList).to.be.sorted(Intl.Collator().compare);
+  describe('Organization list', function () {
+    it('is sorted', function () {
+      var orgList = srcMd.match(/(?<=<summary>)(.*?)(?=<\/summary>)/g);
+      expect(orgList).to.be.sorted(Intl.Collator().compare);
+    });
   });
 });
